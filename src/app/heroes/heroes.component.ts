7e01462363ff8cc0fbee627b72aab072d30a7330
Théo Boulaire <th.boulaire@gmail.com>
2024-03-14 09:15:40 +0100

    Heroes service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,12 +8,25 @@ import { HEROES } from '../mock-heroes';
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   hero: Hero = {
     id: 0,
     name: 'WindStorm',
   };
   selectedHero?: Hero;
+
+  constructor(private heroService: HeroService) {}
+
+  aknowledgeHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => {
+      this.heroes = heroes;
+    });
+  }
+
+  ngOnInit(): void {
+    this.aknowledgeHeroes();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
